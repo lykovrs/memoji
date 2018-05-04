8e8b1434f10a851d890d03ea0b1d5fb8bb257b7c
Roman Lykov <lykovrs@gmail.com>
2018-05-05 00:02:09 +0300

    add ranom dom element logic

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
-function memoji(rootElement, openCardClass) {
-    // делегируем клик
-    rootElement.addEventListener('click', function (ev) {
-        var control = ev.target;
-        // выбираем только целевый элементы-карточки
-        if('card' in control.dataset) {
-            console.log(ev.target)
-            control.classList.toggle(openCardClass)
-        }
+function memoji(rootElement, classCard, openCardClass) {
+  var emojies = [
+    "🦀",
+    "🐟",
+    "🐊",
+    "🐓",
+    "🦃",
+    "🐿",
+    "🦀",
+    "🐟",
+    "🐊",
+    "🐓",
+    "🦃",
+    "🐿",
+  ];
+  // Создаем случайную последовательность эмоджи
+  emojies = shuffle(emojies);
+  // Готовим заготовку карточки
+  var element = document.createElement("article");
+  element.dataset.card = "";
+  element.classList.add(classCard);
+  // Добавляем рандомную последовательность карточек на страницу
+  emojies.forEach(function(item) {
+    var newItem = element.cloneNode(false);
+    newItem.dataset.emoji = item;
+    rootElement.appendChild(newItem);
+  });
 
-    })
+  // делегируем клик
+  rootElement.addEventListener("click", function(ev) {
+    var control = ev.target;
+    // выбираем только целевый элементы-карточки
+    if ("card" in control.dataset) {
+      console.log(ev.target);
+      control.classList.toggle(openCardClass);
+    }
+  });
+
+  /**
+   * Функция перемешивания массива
+   * @param array входной массив
+   * @returns {*}
+   */
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
 }
